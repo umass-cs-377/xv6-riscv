51ef794b1194fd48aaaac9b5fabc6a8c75efddae
SiCh3 <sichircu@gmail.com>
2022-12-09 11:18:01 -0500

    added pstat struct

--- kernel/proc.h
+++ kernel/proc.h
@@ -105,3 +105,14 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
 };
+
+//for mlfq process info:
+struct pstat {
+  int inuse[NPROC]; // whether this slot of the process table is in use (1 or 0)
+  int pid[NPROC];   // PID of each process
+  int priority[NPROC];  // current priority level of each process (0-3)
+  enum procstate state[NPROC];  // current state (e.g., SLEEPING or RUNNABLE) of each process
+  int ticks[NPROC][4];  // number of ticks each process has accumulated 
+			// RUNNING/SCHEDULED at each of 4 priorities
+  int wait_ticks[NPROC][4]; // number of ticks each process has waited before being scheduled
+};
