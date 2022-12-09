21b462c49402c7b44b734092486dd37942740360
wilsonnexus <96637419+wilsonnexus@users.noreply.github.com>
2022-12-09 15:18:31 -0500

    Final Project

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -20,3 +20,5 @@
 #define SYS_link   19
 #define SYS_mkdir  20
 #define SYS_close  21
+// WILSON
+#define SYS_getpinfo 22
--- user/usys.pl
+++ user/usys.pl
@@ -36,3 +36,5 @@ entry("getpid");
 entry("sbrk");
 entry("sleep");
 entry("uptime");
+# WILSON
+entry("getpinfo");
