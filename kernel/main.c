af442772f821480df71b07b947cd5cba5a71d2de
Tim Richards <richards@cs.umass.edu>
2023-04-18 00:00:15 +0000

    feat: docker dev container support

--- kernel/main.c
+++ kernel/main.c
@@ -8,17 +8,15 @@
 volatile static int started = 0;
 
 // start() jumps here in supervisor mode on all CPUs.
-void
-main()
-{
-  if(cpuid() == 0){
+void main() {
+  if (cpuid() == 0) {
     consoleinit();
     printfinit();
     printf("\n");
     printf("xv6 kernel is booting\n");
     printf("\n");
-    kinit();         // physical page allocator
-    kvminit();       // create kernel page table
+    kinit();    // physical page allocator
+    kvminit();  // create kernel page table
     kvminithart();   // turn on paging
     procinit();      // process table
     trapinit();      // trap vectors
@@ -27,9 +25,9 @@ main()
     plicinithart();  // ask PLIC for device interrupts
     binit();         // buffer cache
     iinit();         // inode table
-    fileinit();      // file table
-    virtio_disk_init(); // emulated hard disk
-    userinit();      // first user process
+    fileinit();          // file table
+    virtio_disk_init();  // emulated hard disk
+    userinit();          // first user process
 
     // https://stackoverflow.com/questions/982129/what-does-sync-synchronize-do
     // This basically ensures that the compiler does not move the assignment
@@ -38,16 +36,18 @@ main()
     // thing.
     __sync_synchronize();
     started = 1;
+
   } else {
     // All other harts spin until hart 0 is finished initializing above.
-    while(started == 0)
+    while (started == 0)
       ;
     __sync_synchronize();
+
     printf("hart %d starting\n", cpuid());
-    kvminithart();    // turn on paging
-    trapinithart();   // install kernel trap vector
-    plicinithart();   // ask PLIC for device interrupts
+    kvminithart();   // turn on paging
+    trapinithart();  // install kernel trap vector
+    plicinithart();  // ask PLIC for device interrupts
   }
 
-  scheduler();        
+  scheduler();
 }
